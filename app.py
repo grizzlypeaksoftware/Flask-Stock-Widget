fad53fac6899c73f759b41cd60bdf441f9988467
Shane Larson <shane@grizzlypeaksoftware.com>
2020-01-27 20:20:33 -0900

    Commit for first article publish

--- app.py
+++ app.py
@@ -1,28 +1,45 @@
 from flask import Flask, request, render_template
 import yfinance as yf
 
+# instantiate the Flask app.
 app = Flask(__name__)
 
+# API Route for pulling the stock quote
 @app.route("/quote")
 def display_quote():
+	# get a stock ticker symbol from the query string
+	# default to AAPL
 	symbol = request.args.get('symbol', default="AAPL")
+
+	# pull the stock quote
 	quote = yf.Ticker(symbol)
+
+	#return the object via the HTTP Response
 	return quote.info
 
+# API route for pulling the stock history
 @app.route("/history")
 def display_history():
+	#get the query string parameters
 	symbol = request.args.get('symbol', default="AAPL")
 	period = request.args.get('period', default="1y")
 	interval = request.args.get('interval', default="1mo")
 
+	#pull the quote
 	quote = yf.Ticker(symbol)	
+	#use the quote to pull the historical data from Yahoo finance
 	hist = quote.history(period=period, interval=interval)
+	#convert the historical data to JSON
 	data = hist.to_json()
+	#return the JSON in the HTTP response
 	return data
 
+# This is the / route, or the main landing page route.
 @app.route("/")
 def home():
+	# we will use Flask's render_template method to render a website template.
     return render_template("homepage.html")
 
+# run the flask app.
 if __name__ == "__main__":
 	app.run(debug=True)
--- static/scripts/site.js
+++ static/scripts/site.js
@@ -10,21 +10,27 @@ Site.prototype.Init = function(){
 };
 
 Site.prototype.GetQuote = function(){
-
+	// store the site context.
 	var that = this;
+
+	// pull the HTTP REquest
 	$.ajax({
 		url: "/quote?symbol=" + that.symbol,
 		method: "GET",
 		cache: false
 	}).done(function(data) {
+
+		// set up a data context for just what we need.
 		var context = {};
 		context.shortName = data.shortName;
 		context.symbol = data.symbol;
 		context.price = data.ask;
+
 		if(data.quoteType="MUTUALFUND"){
 			context.price = data.previousClose
 		}		
 
+		// call the request to load the chart and pass the data context with it.
 		that.LoadChart(context);
 	});
 };
@@ -33,12 +39,6 @@ Site.prototype.SubmitForm = function(){
 	this.symbol = $("#symbol").val();
 	this.GetQuote();
 }
-
-Site.prototype.RenderWidget = function(data){
-	var source = $("#module_template").html();
-	var template = Handlebars.compile(source);	
-	$("#widgetContent").html(template(data));
-};
 
 Site.prototype.LoadChart = function(quote){
 
@@ -57,7 +57,6 @@ Site.prototype.RenderChart = function(data, quote){
 	var dates = [];
 
 	var title = quote.shortName  + " (" + quote.symbol + ") - " + numeral(quote.price).format('$0,0.00');
-
 	
 	for(var i in data.Close){
 		var dt = i.slice(0,i.length-3);
@@ -70,27 +69,22 @@ Site.prototype.RenderChart = function(data, quote){
 	}
 	
 	Highcharts.chart('chart_container', {
-
 		title: {
 			text: title
-		},
-	
+		},	
 		yAxis: {
 			title: {
 				text: ''
 			}
-		},
-	
+		},	
 		xAxis: {
 			categories :dates,
-		},
-	
+		},	
 		legend: {
 			layout: 'vertical',
 			align: 'right',
 			verticalAlign: 'middle'
-		},
-	
+		},	
 		plotOptions: {
 			series: {
 				label: {
@@ -99,15 +93,13 @@ Site.prototype.RenderChart = function(data, quote){
 			},
 			area: {
 			}
-		},
-	
+		},	
 		series: [{
 			type: 'area',
 			color: '#85bb65',
 			name: 'Price',
 			data: priceData
-		}],
-	
+		}],	
 		responsive: {
 			rules: [{
 				condition: {
@@ -127,10 +119,8 @@ Site.prototype.RenderChart = function(data, quote){
 
 };
 
-
 var site = new Site();
 
 $(document).ready(()=>{
-	site.RenderWidget({});
 	site.Init();
 })
